a9d495b9a85d75425dbe11cd8dd797db02dd7b64
Sepuri30110 <furious30110@gmail.com>
2026-02-21 16:57:19 +0530

    Added Router in Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,25 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import { ToastContainer } from 'react-toastify';
+
+import PrivateRoute from './PrivateRoutes/PrivateRoute';
+
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      Hi
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<>Home Page</>} />
+        <Route exact path="/login" element={<>Login Page</>} />
+        <Route exact path="/signup" element={<>Signup Page</>} />
+        <Route element={<PrivateRoute/>}>
+          <Route exact path="/user" element={<>User Page</>} />
+          <Route exact path="/profile" element={<>Profile Page</>} />
+        </Route>
+      </Routes>
+      <ToastContainer />
+    </BrowserRouter>
   )
 }
 
